ad1dfbb3db78a340939c0da55a1784ce9ebaba21
Miguel angel peña Guzman <150266608+mangeluke@users.noreply.github.com>
2024-03-23 17:40:48 -0500

    Add files via upload

---
+++ OneDrive/Documents/ejerciciospyton/secuenciales/ejercicio1.py
@@ -0,0 +1,16 @@
+x=2
+y=3
+x= int(input("digite el valor para la variable x: "))
+y= int(input("digite el valor para la variable y: "))
+#operadores para las variables
+suma=x+y
+resta=x-y
+producto=x*y
+divicion=x/y
+modulo=x%y
+print("la respuesta de suma es: ",suma)
+print("la respuesta de la resta es: ",resta)
+print("la respuestas de la producto es: ",producto)
+print("la respuesta de la divicion es: ",divicion)
+print("la respuesta del modulo es: ",modulo)
+print(f"la resta de {2} y {3} es{resta}")
