e0a481589a58f3be2970d3e861496d0e2f7a0a5d
Miguel angel peña Guzman <150266608+mangeluke@users.noreply.github.com>
2024-03-23 17:39:42 -0500

    Add files via upload

--- OneDrive/Documents/ejerciciospyton/repetitivas/ejercicio1.py
+++ OneDrive/Documents/ejerciciospyton/repetitivas/ejercicio1.py
@@ -1,4 +1,28 @@
-text = "hola"
-num = 1 < 5
-num2 = 5 <= 4
-print(num2)
+nota=int(input("ingrese la nota"))
+nota=int(input("ingrese la nota"))
+nota=int(input("ingrese la nota"))
+nota=int(input("ingrese la nota"))
+nota=int(input("ingrese la nota"))
+nota=int(input("ingrese la nota"))
+nota=int(input("ingrese la nota"))
+nota=int(input("ingrese la nota"))
+nota=int(input("ingrese la nota"))
+nota=int(input("ingrese la nota"))
+
+for i in range (10):
+    nota = (input(f"ingrese la calificacion del estudiante {i + 1}: "))
+    if nota < 50:
+        nota += 1
+    elif 50 <= nota < 70:
+        nota += 1
+    elif 70 <= nota < 80:
+        nota += 1
+    else:
+        nota += 1
+
+    
+print(f"numero de calificacion con calificacion menor a 50:{nota}")   
+print(f"numero de calificacion con calificacion 50 y 70:{nota}")
+print(f"numero de calificacion con calificacion 70 y 80:{nota}")
+print(f"numero de calificacion con calificacion mayor a 80:{nota}")
+
